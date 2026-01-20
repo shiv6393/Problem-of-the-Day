0c4f01758bc81433d7a32e277b67eecc1d2d54c3
shiv6393 <shivampandey02080@gmail.com>
2026-01-20 16:13:18 +0530

    Optimal Solution

--- Solution20thjanPod.java
+++ Solution20thjanPod.java
@@ -1,6 +1,28 @@
  import  java.util.*;
  public class Solution20thjanPod {
-     public int[] minBitwiseArray(List<Integer> nums) {
+    //*Brute Force Solution */
+    //  public int[] minBitwiseArray(List<Integer> nums) {
+    //     int n=nums.size();
+    //     int ans[]=new int[n];
+    //     for(int i=0;i<n;i++){
+    //         if(nums.get(i)==2){
+    //             ans[i]=-1;
+    //             continue;}
+    //         for(int j=0;j<32;j++){
+    //             if((nums.get(i) & (1<<j))!=0){
+    //                 continue;
+    //                 }
+    //             else {
+    //                 ans[i]=nums.get(i) ^(1<<j-1);
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     return ans;
+        
+    // }
+    // Optimal Solution
+ public int[] minBitwiseArray(List<Integer> nums) {
         int n=nums.size();
         int ans[]=new int[n];
         for(int i=0;i<n;i++){
@@ -20,5 +42,6 @@
         return ans;
         
     }
+
     
 }
